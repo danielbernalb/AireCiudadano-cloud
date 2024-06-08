2fa7758db50e27c12370d7cb84e875c3430fb705
danielbernalb <danielbernalb@gmail.com>
2024-06-08 15:47:38 -0500

    cambios para NS

--- stack/user_data.sh
+++ stack/user_data.sh
@@ -12,7 +12,7 @@ export GRAFANA_ADMIN_PASSWORD="daniel2022"
 
 #==============Initialize /data if needed===================
 #Ensure there is a directory created for the applications persistent data
-for application in prometheus pushgateway grafana mosquitto letsencrypt
+for application in prometheus pushgateway grafana mosquitto letsencrypt thanos
 do
   if [ ! -d /data/$application ]; then
     sudo mkdir -p /data/$application
@@ -35,7 +35,7 @@ echo "alias helm='microk8s.helm3'" >> $HOME/.bashrc
 
 #================Install anaire cloud stack=================
 cd $HOME
-git clone --branch cambios22NS_27mar2024 https://github.com/danielbernalb/aireciudadano-cloud.git
+git clone --branch cambios22NS_8junio2024 https://github.com/danielbernalb/aireciudadano-cloud.git
 ln -s anaire-cloud/stack/virtualbox/delete_stack.sh
 ln -s anaire-cloud/stack/virtualbox/upgrade_stack.sh
 ln -s anaire-cloud/stack/virtualbox/start_stack.sh
