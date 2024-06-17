7343cab5edf65fe1ba6f0112370c82f85a1630da
danielbernalb <danielbernalb@gmail.com>
2024-06-17 10:24:34 -0500

    branch

--- stack/user_data.sh
+++ stack/user_data.sh
@@ -35,7 +35,7 @@ echo "alias helm='microk8s.helm3'" >> $HOME/.bashrc
 
 #================Install anaire cloud stack=================
 cd $HOME
-git clone --branch cambios25_29mayo2024 https://github.com/danielbernalb/aireciudadano-cloud.git
+git clone --branch cambios26_17junio2024 https://github.com/danielbernalb/aireciudadano-cloud.git
 ln -s anaire-cloud/stack/virtualbox/delete_stack.sh
 ln -s anaire-cloud/stack/virtualbox/upgrade_stack.sh
 ln -s anaire-cloud/stack/virtualbox/start_stack.sh
